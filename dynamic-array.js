8593eae8798cc618a2daa2c9c021c0d58cff6e29
SeymaMolnar <seyma.ercan@gmail.com>
2024-04-07 21:15:07 -0400

    final commit

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,47 +1,72 @@
 class DynamicArray {
-
   constructor(defaultSize = 4) {
-
-    // Your code here 
+    this.data = new Array(defaultSize);
+    this.length = 0;
+    this.capacity = defaultSize;
   }
 
   read(index) {
-
-    // Your code here 
+    if (index < 0 || index >= this.length) return undefined;
+    return this.data[index];
   }
 
   push(val) {
-
-    // Your code here 
+    if (this.length === this.capacity) {
+      this.resize(this.capacity * 2); // Double the capacity
+    }
+    this.data[this.length] = val;
+    this.length++;
   }
 
-
   pop() {
-
-    // Your code here 
+    if (this.length === 0) return undefined;
+    const poppedValue = this.data[this.length - 1];
+    this.data[this.length - 1] = undefined; // Remove the last element
+    this.length--;
+    return poppedValue;
   }
 
   shift() {
-
-    // Your code here 
+    if (this.length === 0) return undefined;
+    const shiftedValue = this.data[0];
+    for (let i = 0; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1]; // Shift elements to the left
+    }
+    this.data[this.length - 1] = undefined; // Remove the last element
+    this.length--;
+    return shiftedValue;
   }
 
   unshift(val) {
-
-    // Your code here 
+    if (this.length === this.capacity) {
+      this.resize(this.capacity * 2); // Double the capacity
+    }
+    // Shift existing elements to the right to make space for the new element
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val; // Add the new element at the beginning
+    this.length++;
   }
 
   indexOf(val) {
-
-    // Your code here 
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === val) {
+        return i;
+      }
+    }
+    return -1; // Element not found
   }
 
-  resize() {
-
-    // Your code here 
+  resize(newCapacity) {
+    const doubledCapacity = newCapacity || this.capacity * 2;
+    const newData = new Array(doubledCapacity);
+    for (let i = 0; i < this.length; i++) {
+      newData[i] = this.data[i]; // Copy existing elements
+    }
+    this.data = newData;
+    this.capacity = doubledCapacity; // Update the capacity property
   }
-
 }
-
 
 module.exports = DynamicArray;
